b721856e1dd2e568972ca0edf80adc3dcb61f3d9
Jay <1356573671@qq.com>
2020-08-09 16:26:51 +0800

    Feat: 修改product表结构

--- models/bmyx/product.js
+++ models/bmyx/product.js
@@ -10,7 +10,7 @@ const product = sequelize.defineModel('product', {
     }, // 产品名字
     nowPrice: Sequelize.DECIMAL(10, 2), // 今天价格
     oldPrice: Sequelize.DECIMAL(10, 2), // 昨天价格
-    imgSrc: Sequelize.STRING, // 图片
+    imgSrcList: Sequelize.STRING, // 图片列表
     detail: Sequelize.STRING // 介绍
 })
 
