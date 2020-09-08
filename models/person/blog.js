d83c7a59c6b87077a82af1e15e18626f1790058a
Jay <zhi-jie.wang@mail.foxconn.com>
2020-09-08 17:41:42 +0800

    feat: 增加博客模块

---
+++ models/person/blog.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../../sqlConfig/dbConn');
+
+const blog = sequelize.defineModel('blog', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    publishTime: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    href: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    imgSrc: DataTypes.STRING,
+    digest: DataTypes.STRING
+})
+
+blog.sync({
+    force: true
+});
+
+module.exports = blog;
