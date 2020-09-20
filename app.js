a92d182db729f24f510d4487aea91dae98b4f21c
Jay <1356573671@qq.com>
2020-09-20 11:35:32 +0800

    feat: 增加token忽略列表

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const cors = require('koa2-cors')
 const tokenFn = require('./util/token')
-const session = require('koa-session');
+const session = require('koa-session')
 const sessionConfig = require('./config/session.config')
 const secret = require('./config/secret')
-const { logger_koa4, accessLogger } = require('./config/logger');
+const { logger_koa4, accessLogger } = require('./config/logger')
 
 // route 引入部分 start ---------------------------------------------------
 const index = require('./routes/index')
@@ -27,82 +27,95 @@ const account = require('./routes/account/Account')
 const verificationCode = require('./routes/verification-code')
 // route 引入部分 end ---------------------------------------------------
 
-app.keys = [secret];   /*cookie的签名*/
+app.keys = [secret] /*cookie的签名*/
 
 //配置session的中间件
-app.use(session(sessionConfig, app));  // maxAge--cookie的过期时间
+app.use(session(sessionConfig, app)) // maxAge--cookie的过期时间
 
 // error handler
 onerror(app)
 
 // middlewares
-app.use(bodyparser({
-    enableTypes: ['json', 'form', 'text']
-}))
+app.use(
+	bodyparser({
+		enableTypes: ['json', 'form', 'text']
+	})
+)
 app.use(json())
 app.use(logger())
-app.use(accessLogger());
+app.use(accessLogger())
 app.use(require('koa-static')(__dirname + '/public'))
 
-app.use(views(__dirname + '/views', {
-    extension: 'ejs'
-}))
+app.use(
+	views(__dirname + '/views', {
+		extension: 'ejs'
+	})
+)
 
 // logger
 app.use(async (ctx, next) => {
-    const start = new Date()
-    await next();
-    tokenFilter(ctx);   // token 验证
-    const ms = new Date() - start
-    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+	const start = new Date()
+	await next()
+	tokenFilter(ctx) // token 验证
+	const ms = new Date() - start
+	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
 /**
  * token 验证
  */
 async function tokenFilter(ctx) {
-    // 过滤 OPTIONS 请求
-    if (ctx.method == 'OPTIONS') {
-        return false;
-    }
-    let url = ctx.url;
-    let allowpage = [
-        '/api/account/login',
-        '/api/bmyx/findBySearch',
-        '/api/bmyx/getProductsByCond',
-        '/api/bmyx/getData',
-        '/api/bmyx/getSort',
-        '/api/bmyx/getNotice',
-        '/api/bmyx/wx_login',
-        "/api/verification-code/getCode"
-    ];
-    let token = ctx.header.authorization;
-    if (url.split('/')[1] === 'api' && allowpage.indexOf(url.split('?')[0]) <= -1) {
-        if (await tokenFn.isAuthorization(token)) {
-            ctx.body = {
-                code: 401,
-                msg: "token 验证失败",
-            }
-        }
-    }
+	// 过滤 OPTIONS 请求
+	if (ctx.method == 'OPTIONS') {
+		return false
+	}
+	let url = ctx.url
+	let allowpage = [
+		'/api/account/login',
+		'/api/bmyx/findBySearch',
+		'/api/bmyx/getProductsByCond',
+		'/api/bmyx/getData',
+		'/api/bmyx/getSort',
+		'/api/bmyx/getNotice',
+		'/api/bmyx/wx_login',
+		'/api/verification-code/getCode',
+		'/api/person/getBasicinfo',
+		'/api/person/getExperiences',
+		'/api/person/getExperiences',
+		'/api/person/getBlogs',
+		'/api/person/createMsg'
+	]
+	let token = ctx.header.authorization
+	if (
+		url.split('/')[1] === 'api' &&
+		allowpage.indexOf(url.split('?')[0]) <= -1
+	) {
+		if (!token || (await tokenFn.isAuthorization(token))) {
+			ctx.body = {
+				code: 401,
+				msg: 'token 验证失败'
+			}
+		}
+	}
 }
 
 // 配置跨域
 app.use(
-    cors({
-        origin: function (ctx) { //设置允许来自指定域名请求
-            // if (ctx.url === '/test') {
-            //     return '*'; // 允许来自所有域名请求
-            // }
-            return ctx.header.origin; //只允许http://localhost:8080这个域名的请求
-        },
-        maxAge: 5, //指定本次预检请求的有效期，单位为秒。
-        credentials: true, //是否允许发送Cookie
-        allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
-        allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
-        exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
-    })
-);
+	cors({
+		origin: function (ctx) {
+			//设置允许来自指定域名请求
+			// if (ctx.url === '/test') {
+			//     return '*'; // 允许来自所有域名请求
+			// }
+			return ctx.header.origin //只允许http://localhost:8080这个域名的请求
+		},
+		maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+		credentials: true, //是否允许发送Cookie
+		allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
+		allowHeaders: ['Content-Type', 'Authorization', 'Accept'], //设置服务器支持的所有头信息字段
+		exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] //设置获取其他自定义字段
+	})
+)
 
 // routes 路由配置
 app.use(index.routes(), index.allowedMethods())
@@ -126,8 +139,8 @@ app.use(bmyx_sortOfProduct.routes(), bmyx_sortOfProduct.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-    logger_koa4.error(err);
-    console.error('server error', err, ctx)
-});
+	logger_koa4.error(err)
+	console.error('server error', err, ctx)
+})
 
 module.exports = app
